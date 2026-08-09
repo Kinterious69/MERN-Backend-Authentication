d3bb8b3d5d4e6e7a1ac8ffbd6437757a7fe14eef
sulayman <sulaymankinteh918@gmail.com>
2026-08-09 22:56:36 +0000

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 // 1. FIXED: Removed the trailing slash "/" from the URL string
-const frontEndApi = ["https://mern-backend-authentication.vercel.app","https://vercel.com/sulayman-kintehs-projects/mern-backend-authentication/EtEDUvxuwqB65nkKCVFfhHJybGwT"]
+const frontEndApi = ["https://mern-backend-authentication.vercel.app","https://mern-backend-authentication-o7kj81ccv-sulayman-kintehs-projects.vercel.app"]
 
 // 2. CORS Middleware (Must stay at the top)
 app.use(cors({
