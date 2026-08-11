3e3856bc96053c80701d52a8715aa61e30a04885
sulayman <sulaymankinteh918@gmail.com>
2026-08-11 17:27:03 +0000

    fix frontend navbar issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
        <div className='group '>
                   {
                      userData ?(
-                      <><button  className=' py-1.5 px-5  text-xs relative shadow-lg font-bold bg-transparent rounded-full ' >{userData&& userData.name[0].toUpperCase()}</button>
+                      <><button  className=' py-2 px-3  text-xs relative shadow-lg font-bold bg-transparent rounded-full ' >{userData&& userData.name[0].toUpperCase()}</button>
                       <ul className='group-hover:block   hidden bg-transparent cursor-pointer absolute right-0 mr-3 p-1 text-sm'>
                        <li onClick={handleVerifyOtp} className='hover:bg-white text-xs hover:text-black mb-1'>verifyEmail</li> 
                         <li onClick={handleClick} className='hover:bg-white text-xs hover:text-black'>Logout</li>
@@ -80,7 +80,7 @@ const Navbar = () => {
                         </>
                      ):
                      (
-                        <button onClick={()=>navigate("/")} className=' py-1.5 px-5  text-xs relative shadow-lg font-bold bg-transparent rounded-full tracking-wider' >login</button>
+                        <button onClick={()=>navigate("/")} className='   text-xs relative shadow-lg font-bold bg-transparent px-3 py-2 rounded-full tracking-wider' >A</button>
                      )
                   }    
 
