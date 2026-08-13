1d70a10cf1848b00c6580d650b1972965d0fce9c
sulayman <sulaymankinteh918@gmail.com>
2026-08-13 13:55:17 +0000

    improved reset password otp handling middleware

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -89,7 +89,7 @@ const ResetPassword = () => {
                  e.preventDefault()
                  const finalOtp = inputRef.current.filter(Boolean).map(e => e.value).join("");
                 try{
-                    const {data} = axios.post("/api/auth/verifyOtp", {email})
+                    const {data} = await axios.post("/api/auth/verifyOtp", {email})
                     if(data.success){
                          setOtp(finalOtp); 
                          setIsOtpInputSent(true)
